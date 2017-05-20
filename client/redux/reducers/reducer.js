4c00c89b0c039f52aa9aaf2e9f7ec21c7305e79e
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-20 17:26:33 -0500

    Add redux boilerplate

--- client/redux/reducers/reducer.js
+++ client/redux/reducers/reducer.js
@@ -1,1 +1,10 @@
 // reducer.js
+const reducer = (state = 'Initial State', action) => {
+  switch (action.type) {
+    case 'CHANGE_STATE':
+      return action.data;
+    default:
+      return state;
+  }
+};
+export default reducer;
