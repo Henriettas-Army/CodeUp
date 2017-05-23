36f3208f1c0da141447d7d940ff889291323b554
gjblanco <gjblanco92@gmail.com>
2017-05-23 16:34:53 -0500

    refactor eventActions.js

--- client/redux/actions/eventActions.js
+++ client/redux/actions/eventActions.js
@@ -1,60 +1,76 @@
 const axios = require('axios');
 
-/* Actions */
-const POST_EVENT = 'POST_EVENT';
-const FETCH_EVENTS = 'GET_EVENTS';
-const LOADING_EVENTS = 'LOADING_EVENTS';
-const ERROR_EVENTS = 'ERROR_EVENTS';
-const SUCCESS_EVENTS = 'SUCCESS_EVENTS';
+/* Action Types */
+const LOADING = 'LOADING';
+const ERROR = 'ERROR';
+const SUCCESS = 'SUCCESS';
+
+/* STATUS */
+const StatusEnum = {
+  LOADING: 1,
+  READY: 2,
+  ERROR_LOADING: 4,
+  ERROR_POSTING: 8,
+};
+
+// add validation errors??
 
 /* Action Creators */
 
-const errorEvents = err => ({ type: ERROR_EVENTS, error: err });
-const loadingEvents = () => ({ type: LOADING_EVENTS });
-const successEvents = events => ({ type: SUCCESS_EVENTS, events });
+const errorEvents = (errorType, error) => ({ type: ERROR, errorType, error });
+const loadingEvents = () => ({ type: LOADING });
+const successEvents = events => ({ type: SUCCESS, events });
 
-const urlGetEvents = '';
-const urlPostEvents = '';
+const urlGetEvents = '/api/events';
+const urlPostEvents = '/api/events';
 
-const fetchEvents = () => (dispatch) => {
+const loadEventsAsync = () => (dispatch) => {
   dispatch(loadingEvents());
 
   axios.get(urlGetEvents)
     .then((response) => {
       if (response.ok === false) {
-        dispatch(errorEvents(response.err));
+        dispatch(errorEvents(StatusEnum.ERROR_LOADING, response.err));
       } else {
         dispatch(successEvents(response.events));
       }
     });
 };
 
-const postEvent = event => (dispatch) => {
+const postEventAsync = event => (dispatch) => {
   dispatch(loadingEvents());
 
   axios.post(urlPostEvents, event)
-    .then((response) => { 
+    .then((response) => {
       if (response.ok !== true) {
         throw new Error('Error posting event');
       }
       return response;
     })
     .then(() => {
-      dispatch(fetchEvents());
+      dispatch(loadEventsAsync());
     })
     .catch((err) => {
-      dispatch(errorEvents(err));
+      dispatch(errorEvents(StatusEnum.ERROR_POSTING, err));
     });
-}
+};
 
 module.exports = {
-  POST_EVENT,
-  FETCH_EVENTS,
-  LOADING_EVENTS,
-  ERROR_EVENTS,
-  postEvent,
-  fetchEvents,
+  /** Action Types */
+  SUCCESS,
+  ERROR,
+  LOADING,
+
+  /** Action Creators */
   errorEvents,
   loadingEvents,
+  successEvents,
+
+  /** Async Action Creators */
+  postEventAsync,
+  loadEventsAsync,
+
+  /** Status Values Enum */
+  StatusEnum,
 };
 
