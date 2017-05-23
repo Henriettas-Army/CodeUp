3cb3d2018c3b9ec973b3b483f1a4433214910fbc
Doris <chihyundorischiu@gmail.com>
2017-05-23 18:10:17 -0500

    (feat) retrieve all user repos and user info

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 // const passport = require('passport');
 // const passportJWT = require('passport-jwt');
 // const jwt = require('jsonwebtoken');
-
+const utils = require('./utils.js');
 const db = require('./../db/config.js');
 // const users = require('./routes/users.js');
 
@@ -19,6 +19,21 @@ app.use(express.static(path.join(__dirname, './../client')));
 app.listen('3034', () => {
   console.log('Listening on port 3034...');
 });
+
+// const options = {
+//   uri: 'https://api.github.com/users/cdcjj',
+//   headers: {
+//     'User-Agent': 'Request-Promise',
+//   },
+//   json: true,
+// };
+
+// https://github.com/${username};
+// https://api.github.com/${username}/repos;
+utils.gitUserRepos('techmexdev')
+  .then((repos) => {
+    console.log(repos);
+  });
 
 module.exports = {
   app,
