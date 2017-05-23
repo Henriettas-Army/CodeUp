10c6b288d2f655f8110fc3ce2c28ecda72e2ecb3
gjblanco <gjblanco92@gmail.com>
2017-05-23 08:53:28 -0500

    irrelevant commit

--- client/redux/actions/actions.js
+++ client/redux/actions/actions.js
@@ -4,14 +4,14 @@ const POST_EVENT = 'POST_EVENT';
 const GET_EVENTS = 'GET_EVENTS';
 
 const changeState = newState => ({ type: CHANGE_STATE, newState });
-const postEventAsync = event => //({ type: POST_EVENT, event });
-const getEventsAsync = () => dispatch => //({ type: GET_EVENTS });
+const postEvent = event => ({ type: POST_EVENT, event });
+const getEvents = () => ({ type: GET_EVENTS });
 
 module.exports = {
   CHANGE_STATE,
   POST_EVENT,
   GET_EVENTS,
   changeState,
-  postEventAsync,
-  getEventsAsync,
+  postEvent,
+  getEvents,
 };
