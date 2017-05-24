9ad8e79feb3128179a04af28b8a42015b414023d
Adam Alcott <aalcott14@gmail.com>
2017-05-24 16:53:34 -0500

    replace promise with axios and receiving access token from git

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 // mongoose.connect('mongodb://db:27017');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb:localhost/codeupdb');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -29,6 +29,15 @@ const SECRET = GITHUB_API.CLIENT_SECRET;
 router.get('/login', (req, res) => {
   const CODE = req.query.code;
   console.log('CODE:', CODE);
+  axios(`https://github.com/login/oauth/access_token?client_id=${ID}&redirect_uri=http://localhost:3034/api/users/login/oauth_redirect&client_secret=${SECRET}&code=${CODE}`)
+  .then((response) => {
+    console.log('TOKEN:', response.data.split('&')[0].split('=')[1]);
+    const TOKEN = response.data.split('&')[0].split('=')[1];
+    axios(`https://api.github.com/user?access_token=${TOKEN}`)
+    .then((resp) => {
+      console.log(resp);
+    });
+  });
 });
 >>>>>>> storing code received from github
 
