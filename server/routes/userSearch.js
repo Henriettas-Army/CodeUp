93e8ef11bf993c1d6d8d6955ce4eee6287edaee6
Fliko <baxter.flw@gmail.com>
2017-05-25 12:19:47 -0500

    Build basic backend query and add data

--- server/routes/userSearch.js
+++ server/routes/userSearch.js
@@ -1,10 +1,16 @@
 const express = require('express');
-// const userController = require('../../db/controllers/UserController');
+const userController = require('../../db/controllers/UserController');
 
 const router = express.Router();
 
-router.get('/userSearch').then((response) => {
-  response.send('suck it m8!');
+router.get('/userSearch').then((res) => {
+  userController.getAllUsers()
+  .then((resp) => {
+    res.status(200).json(resp);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
