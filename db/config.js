5185c320d01d641bce80b1ae7ba62dc1a29d1f09
gjblanco <gjblanco92@gmail.com>
2017-05-24 19:46:54 -0500

    refactor event form into a new Component

--- db/config.js
+++ db/config.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://db:27017');
+// mongoose.connect('mongodb://db:27017');
+mongoose.connect('mongodb://localhost/test');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
