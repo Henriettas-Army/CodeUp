b2ee81e476c4e85f5451e7900138269fb47b3114
Fliko <baxter.flw@gmail.com>
2017-05-24 19:38:00 -0500

    Combine my reducer

--- client/redux/reducers/combineReducers.js
+++ client/redux/reducers/combineReducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import auth from './loginReducer';
 import profile from './profileReducer';
 import events from './events';
+import users from './userListReducer';
 
 const codeUpApp = combineReducers({
   auth,
   events,
   profile,
+  users,
 });
 
 export default codeUpApp;
