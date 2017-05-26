be59de50f0ad3e2ae53b5f768186ba6862215aa8
Fliko <baxter.flw@gmail.com>
2017-05-26 16:25:39 -0500

    fixing gillermos bad code

--- server/routes/eventsRoute.js
+++ server/routes/eventsRoute.js
@@ -2,7 +2,6 @@ const eventHelper = require('../../db/controllers/eventHelper');
 
 module.exports = {
   postEvent: (req, res) => {
-    console.log('########################', req.body)
     eventHelper.addEvent(req.body)
     .then((event) => {
       res.status(200).json({ event, ok: true });
