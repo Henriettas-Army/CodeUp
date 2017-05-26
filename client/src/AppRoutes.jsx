2a824a15e5d82b048532f683d56cafbf652548e5
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-26 15:03:48 -0500

    Set up react routing

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -4,17 +4,26 @@ import {
   Route,
   hashHistory
 } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import codeUpApp from '../redux/reducers/combineReducers';
 import Search from './components/Search';
 import Login from './components/LoginComponent';
 import './styles/styles.scss';
+import App from './App';
+
+const store = createStore(codeUpApp, applyMiddleware(thunkMiddleware));
 
 const AppRoutes = () => (
-  <Router history={hashHistory}>
-    <div>
-      <Route exact path="/" component={Login}/>
-      <Route path="/search" component={Search} />
-      <Route path="/login" component={Login} />
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/search" component={Search} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
+  </Provider>
 );
 export default AppRoutes;
