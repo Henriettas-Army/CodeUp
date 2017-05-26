6c9ba4bf0d52a20e5fa07d74d364ad8560722114
Adam Alcott <aalcott14@gmail.com>
2017-05-26 18:12:05 -0500

    move animate tags inside login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import GrommetApp from 'grommet/components/App';
-import Animate from 'grommet/components/Animate';
 
 import LoginComponent from './components/LoginComponent';
 import Nav from './containers/Nav';
@@ -11,12 +10,7 @@ import UserList from './containers/Users';
 
 const App = () => (
   <GrommetApp>
-    <Animate
-      enter={{ animation: 'fade', duration: 5000, delay: 0 }}
-      keep
-    >
-      <LoginComponent />
-    </Animate>
+    <LoginComponent />
     <Nav />
     <UserList />
     <Events />
--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import jwtDecode from 'jwt-decode';
 import SocialGithubIcon from 'grommet/components/icons/base/SocialGithub';
+import Animate from 'grommet/components/Animate';
+
 import { loginUser } from '../../redux/actions/loginActions';
 
 const GITHUB_API = require('../../../server/config/github');
@@ -32,16 +34,21 @@ class LoginComponent extends Component {
 
   render() {
     return (
-      <div className="welcome" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <p style={{ fontSize: '80px' }}>Welcome to</p><br />
-        <p style={style}>codeUp</p><br />
-        <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user,repo`}>
-          <SocialGithubIcon
-            size={'xlarge'}
-          /><br />
-          Sign in with GitHub
-        </a>
-      </div>
+      <Animate
+        enter={{ animation: 'fade', duration: 5000, delay: 0 }}
+        keep
+      >
+        <div className="welcome" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <p style={{ fontSize: '80px' }}>Welcome to</p><br />
+          <p style={style}>codeUp</p><br />
+          <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user,repo`}>
+            <SocialGithubIcon
+              size={'xlarge'}
+            /><br />
+            Sign in with GitHub
+          </a>
+        </div>
+      </Animate>
     );
   }
 }
