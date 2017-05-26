a787ce126276d5e65049afa803cd66bdb3f4ccc7
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-25 21:21:20 -0500

    Connect to mongo container

--- db/config.js
+++ db/config.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-
 // mongoose.connect('mongodb://db:27017');
 mongoose.connect('mongodb://localhost/codeupdb');
-// mongoose.connect('mongodb://localhost/test');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
