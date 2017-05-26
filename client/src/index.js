3cb6ecfc1aa59c7a6a558116e27143cc96d23311
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-25 21:08:29 -0500

    Setup non-working routing

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,4 @@ import { createElement } from 'react';
 import { render } from 'react-dom';
 import AppRoutes from './AppRoutes';
 
-render(createElement(AppRoutes), document.getElementById('app'))
+render(createElement(AppRoutes), document.getElementById('app'));
