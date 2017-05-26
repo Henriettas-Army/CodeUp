cdc078a60a963ba91a314293c81608b4745d2f7d
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-25 21:08:29 -0500

    Setup non-working routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,34 +1,6 @@
 /* global document */
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
-import codeUpApp from '../redux/reducers/combineReducers';
-import App from './App';
-import './styles/styles.scss';
+import AppRoutes from './Router';
 
-const store = createStore(codeUpApp, applyMiddleware(thunkMiddleware));
-
-ReactDOM.render(
-  <Router>
-    <div>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <Provider store={store}>
-            <App />
-          </Provider>
-        )}
-      />
-      <Route path="/search" component={Search} />
-      <Route path="/login" component={Login} />
-    </div>
-  </Router>,
-  document.getElementById('app'),
-);
+ReactDOM.render(createElement(AppRoutes), document.getElementById('app'));
