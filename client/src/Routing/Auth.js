bf75a6253377c7c59032a829d3d8a20b04cc6ed2
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-26 18:47:05 -0500

    Add redux integration to PrivateRoute

--- client/src/Routing/Auth.js
+++ client/src/Routing/Auth.js
@@ -1,3 +1,11 @@
 module.exports = {
   isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
 };
