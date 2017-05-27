63347a876be9f3284e26847428fa099fc68e6162
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-27 08:38:30 -0500

    Use prop types from npm in userListComponent

--- client/src/containers/Users.jsx
+++ client/src/containers/Users.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import UserList from '../components/userListComponent';
 import userAction from '../../redux/actions/userListAction';
