1dbc220c8b29f47073ddf41effe8ced5faeab670
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-27 15:37:58 -0500

    Add profile styling

--- client/src/Routing/AppRoutes.jsx
+++ client/src/Routing/AppRoutes.jsx
@@ -9,7 +9,6 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import PrivateRoute from './PrivateRoute';
-import RootRoute from './RootRoute';
 import codeUpApp from '../../redux/reducers/combineReducers';
 import Search from '../components/Search';
 import Login from '../components/LoginComponent';
@@ -25,7 +24,7 @@ const AppRoutes = () => (
     <Router history={hashHistory}>
       <div>
         <Route path="/login" component={Login} />
-        <Route exact path="/" component={RootRoute}/>
+        <Route exact path="/" component={App} />
         <Route path="/profile/:username" component={Profile} />
         <PrivateRoute path="/search" component={Search} />
         <PrivateRoute path="/events" component={Events} />
