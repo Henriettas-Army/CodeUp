be1e030b44ceea1c141a9b57419b2f8631dfc0d4
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-27 16:58:33 -0500

    Add explore route

--- client/src/Routing/AppRoutes.jsx
+++ client/src/Routing/AppRoutes.jsx
@@ -12,7 +12,6 @@ import PrivateRoute from './PrivateRoute';
 import codeUpApp from '../../redux/reducers/combineReducers';
 import Search from '../components/Search';
 import Login from '../components/LoginComponent';
-import App from '../components/App';
 import Events from '../containers/Events';
 import Profile from '../containers/Profile';
 import Explore from '../components/Explore';
@@ -25,7 +24,7 @@ const AppRoutes = () => (
     <Router history={hashHistory}>
       <div>
         <Route path="/login" component={Login} />
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Explore} />
         <Route path="/profile/:username" component={Profile} />
         <PrivateRoute path="/search" component={Search} />
         <PrivateRoute path="/events" component={Events} />
