a1b0f85bd47ea8d176281d84fd6aac182a7b4be4
Doris <chihyundorischiu@gmail.com>
2017-05-30 17:18:31 -0500

    add propTypes validation

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -1,23 +1,12 @@
-<<<<<<< d8d6b45864369a31ee0b21708f90d0cc5da3f11e
 import React from 'react';
 import Tabs from 'grommet/components/Tabs';
 import Tab from 'grommet/components/Tab';
 import Events from '../containers/Events';
 import Users from '../containers/Users';
 import NavContainer from '../containers/NavContainer';
-=======
-/* global window */
-import React, { Component } from 'react';
-import axios from 'axios';
->>>>>>> merge and fix linter errors
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Tabs from 'grommet/components/Tabs';
-import Tab from 'grommet/components/Tab';
-import NavContainer from '../containers/NavContainer';
 import loginActions from '../../redux/actions/loginActions';
-import Events from '../containers/Events';
-import Users from '../containers/Users';
 import Map from '../containers/MapData';
 
 const Explore = () => (
