5a818daee8681574f1968646c4c56cae8cc75ea1
Doris <chihyundorischiu@gmail.com>
2017-05-30 17:31:52 -0500

    change Explore to container

--- client/src/containers/Explore.jsx
+++ client/src/containers/Explore.jsx
@@ -1,9 +1,11 @@
 /* global window */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import Tabs from '../components/Tabs';
 import NavContainer from '../containers/NavContainer';
+import loginAction from '../../redux/actions/loginActions';
 
 class Explore extends Component {
   componentWillMount() {
@@ -42,4 +44,19 @@ Explore.defaultProps = {
   isAuthenticated: '',
 };
 
-export default Explore;
+const mapStateToProps = state => (
+  ({
+    isAuthenticated: state.auth.isAuthenticated,
+  })
+);
+
+const mapDispatchToProps = dispatch => ({
+  loginUser: (username, status) => {
+    dispatch(loginAction.loginUser(username, status));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Explore);
