b5989cb652b9cd503b5a5fa43530bf22ee34f070
Fliko <baxter.flw@gmail.com>
2017-05-31 15:40:00 -0500

    render locations on map view

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tabs from '../components/Tabs';
 import NavContainer from '../containers/NavContainer';
+import Map from '../containers/MapData';
 
 const Explore = () => (
   <div>
@@ -10,7 +11,21 @@ const Explore = () => (
     <br />
     <br />
     <br />
+<<<<<<< 7deb80d8d35f9e9d07296f7fe76b28a82df4b5d3
     <Tabs />
+=======
+    <Tabs>
+      <Tab title="Events">
+        <Events />
+      </Tab>
+      <Tab title="Users">
+        <Users />
+      </Tab>
+      <Tab title="Map">
+        <Map />
+      </Tab>
+    </Tabs>
+>>>>>>> render locations on map view
   </div>
 );
 
