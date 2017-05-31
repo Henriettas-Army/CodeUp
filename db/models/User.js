b5a81683457bf68bfa70a3f5d2b538441665b549
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-31 11:56:58 -0500

    Add endorsement schema

--- db/models/User.js
+++ db/models/User.js
@@ -3,54 +3,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const endorsementSchema = new Schema({
-  endorser_username: String,
-  endorser_img: String,
+  endorser: String,
   skills: [String],
-  comment: String
+  comment: [String]
 });
 
 const userSchema = new Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
+    required: true
   },
-  name: {
-    type: String,
-  },
-  img: {
-    type: String,
-  },
-  bio: {
-    type: String,
-  },
-  skills: {
-    type: [String],
-  },
-  desired: {
-    type: [String],
-  },
-  location: {
-    type: [String],
-  },
-  status: {
-    type: String,
-  },
-  repos: {
-    type: [String],
-  },
-  languages: {
-    type: String,
-  },
-  access_token: {
-    type: String,
-  },
-  meter: {
-    type: [Schema.Types.Mixed],
-  },
-  endorsements: {
-    type: [endorsementSchema]
-  }
+  name: String,
+  img: String,
+  bio: String,
+  skills: [String],
+  desired: [String],
+  location: [String],
+  status: String,
+  languages: String,
+  access_token: String,
+  meter: [Schema.Types.Mixed],
+  endorsement: [endorsementSchema]
 });
 
 const User = mongoose.model('user', userSchema);
