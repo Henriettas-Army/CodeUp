6e23998a001f5d54b51c6b37850f9da6529a8973
Doris <chihyundorischiu@gmail.com>
2017-06-01 14:07:17 -0500

    (feat) preview of app carousel

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 /* global document */
 import { createElement } from 'react';
 import { render } from 'react-dom';
-import AppRoutes from './routing/AppRoutes';
+import AppRoutes from './Routing/AppRoutes';
 
 render(createElement(AppRoutes), document.getElementById('app'));
