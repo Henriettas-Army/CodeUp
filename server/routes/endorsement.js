77948cb726b699c8cda66e15184bb2e43161ec9f
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-31 19:00:11 -0500

    Send endorsement when endorsement was inserted in DB successfully

---
+++ server/routes/endorsement.js
@@ -0,0 +1,13 @@
+const endorsementController = require('../../db/controllers/endorsementController');
+
+const endorse = (req, res) => {
+  endorsementController.postEndorsement(req.body)
+  .then((endorsement) => {
+    res.status(200).json({ "Sent endorsement": endorsement });
+  })
+  .catch((e) => {
+    res.send(e);
+  });
+};
+
+module.exports = endorse;
