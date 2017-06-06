2a96c07f369b3b1a94865704b1154ba75e152dcd
Doris <chihyundorischiu@gmail.com>
2017-06-05 19:01:47 -0500

    restore persist and db

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 // mongoose.connect('mongodb://db:27017');
-mongoose.connect('mongodb://localhost/codeupdb2');
+mongoose.connect('mongodb://localhost/codeupdb');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
