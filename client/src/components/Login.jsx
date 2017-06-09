80029fbe55d8ea883f1a47f29bf12e984b73d8fb
Doris <chihyundorischiu@gmail.com>
2017-06-09 11:47:14 -0500

    add styling to Welcome page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -9,7 +9,7 @@ const GITHUB_API = require('../../../server/config/github');
 const CLIENT_ID = GITHUB_API.CLIENT_ID;
 
 const style = {
-  fontSize: '120px',
+  fontSize: '15vh',
 };
 
 const Login = () => (
@@ -17,21 +17,20 @@ const Login = () => (
     enter={{ animation: 'fade', duration: 5000, delay: 0 }}
     keep
   >
-    <div className="welcome" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <p style={{ fontSize: '80px' }}>Welcome to</p><br />
-      <p style={style}>codeUp</p><br />
+    <div className="welcome" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 0 }}>
+      <p style={style}>codeUp</p>
       <p style={{ textAlign: 'center' }}>
         Find a partner to code with, attend events, and get involved with the coding community
       </p>
+      <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`} style={{ textAlign: 'center' }}>
+        <SocialGithubIcon
+          size={'large'}
+        /><br />
+        <span>Sign in with GitHub</span>
+      </a>
       <Section size="large">
         <PreviewCarousel />
       </Section>
-      <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`}>
-        <SocialGithubIcon
-          size={'xlarge'}
-        /><br />
-        Sign in with GitHub
-      </a>
     </div>
   </Animate>
 );
