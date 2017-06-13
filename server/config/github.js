30830ba3b8bbeac3f439cdcb407ae2304653c860
gjblanco <gjblanco92@gmail.com>
2017-06-13 11:18:22 -0500

    change id and secret to production

--- server/config/github.js
+++ server/config/github.js
@@ -1,4 +1,14 @@
+const dev = {
+  id: '17f3b8efaccd2b18d390',
+  secret: 'd360a97ee3a02f8fce5b32a6c6e39c146847f3d9',
+};
+
+const prod = {
+  id: '24a1410d57938ac7b849',
+  secret: '962dccc1a55b4aa757131712e3a9d5276cf917c3',
+};
+
 module.exports = {
-  CLIENT_ID: '17f3b8efaccd2b18d390',
-  CLIENT_SECRET: 'd360a97ee3a02f8fce5b32a6c6e39c146847f3d9',
+  CLIENT_ID: prod.id,
+  CLIENT_SECRET: prod.secret,
 };
